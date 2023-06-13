d676300e0ee95f9b618a6d4904db06a73edf05d5
furk <helloitsmefurki@gmail.com>
2023-06-14 00:31:14 +0300

    Mockup

--- README.md
+++ README.md
@@ -1,19 +1,20 @@
-# Pythyon Steganography 
+# Huffman Encoding
 
-This approach uses the least significant bit of some selected pixels to store a bit of the secret message.
-This means that the image will be slightly altered, but the human eye will not be able to notice the difference.
+Data Compression Final Project
 
-The pixels that store the secret message are selected by a stepping value, which is the number of pixels that will be skipped before the next pixel is selected.
-This stepping value is selected as next value of pi.
+\
+**Installation:** \
+pip install pillow \
+pip install mpmath
 
-Pi is "3.14159...". So the first stepping value will be 1, the second 4, the third 1, the fourth 5, and so on.
+\
+**Usage:** \
+Usage: python main.py \<x> \<y> --z(optional)
 
-**Installation:**\
-pip install pillow
-
-To **hide** messages into a png image: \
-Usage: python hide.py \<image> \<secret message>
-  
-To **reveal** messages from an encoded png file: \
-Usage: python reveal.py \<image with secrets>
+\
+\
+**Sources:** \
+https://www.geeksforgeeks.org/huffman-coding-greedy-algo-3/ \
+https://www.geeksforgeeks.org/huffman-decoding/ \
+https://www.programiz.com/dsa/huffman-coding
   
--- main.py
+++ main.py
@@ -1,32 +1,96 @@
 import sys
 from PIL import Image, ImageChops
 from mpmath import mp
- 
+
+"""
+# Huffman Coding
+create a priority queue Q consisting of each unique character.
+sort then in ascending order of their frequencies.
+for all the unique characters:
+    create a newNode
+    extract minimum value from Q and assign it to leftChild of newNode
+    extract minimum value from Q and assign it to rightChild of newNode
+    calculate the sum of these two minimum values and assign it to the value of newNode
+    insert this newNode into the tree
+return rootNode
+"""
+
+# Creating tree nodes
+class NodeTree(object):
+
+    def __init__(self, left=None, right=None):
+        self.left = left
+        self.right = right
+
+    def children(self):
+        return self.left, self.right
+
+    def nodes(self):
+        return self.left, self.right
+
+    def __str__(self):
+        return '%s_%s' % (self.left, self.right)
+
+
+# Main function implementing huffman coding
+def huffman_code_tree(node, left=True, binString=''):
+    if type(node) is str:
+        return {node: binString}
+    (l, r) = node.children()
+    d = dict()
+    d.update(huffman_code_tree(l, True, binString + '0'))
+    d.update(huffman_code_tree(r, False, binString + '1'))
+    return d
+
 
 if __name__ == '__main__':
 
-    if len(sys.argv) == 3:
-        flag_opt = False
-    elif (len(sys.argv) == 4) and sys.argv[3].__eq__("--diff"):
-        flag_opt = True
+    if len(sys.argv) == 1:
+        string = "THIS IS GETTING SERIOUSOSSSSSSSSSS"
+        flag_z = False
+    elif len(sys.argv) == 2:
+        string = sys.argv[1]
+        flag_z = False
+    elif (len(sys.argv) == 3) and sys.argv[2].__eq__("--z"):
+        flag_z = True
     else:
         print("ERROR: Invalid arguments!")
-        print("Usage: python main.py <x> <y> --z(optional)")
+        print("Usage: python main.py [string] --z(optional)")
         print("Necessities:")
-        print("       <x> : info.")
-        print("       <y> : info.")
+        print("       [string] : Text.")
         print("Optional Arguments:")
         print("       --z : info.")
         sys.exit(1)
 
-    img_path = sys.argv[1]
-    secretMessage = sys.argv[2]
-
-    # Open the image with RGB mode
-    img = Image.open(img_path).convert('RGB')
 
 
+    # Calculating frequency
+    freq = {}
+    for c in string:
+        if c in freq:
+            freq[c] += 1
+        else:
+            freq[c] = 1
 
-    # Compare the images
-    if flag_opt:
+    freq = sorted(freq.items(), key=lambda x: x[1], reverse=True)
+
+    nodes = freq
+
+    while len(nodes) > 1:
+        (key1, c1) = nodes[-1]
+        (key2, c2) = nodes[-2]
+        nodes = nodes[:-2]
+        node = NodeTree(key1, key2)
+        nodes.append((node, c1 + c2))
+
+        nodes = sorted(nodes, key=lambda x: x[1], reverse=True)
+
+    huffmanCode = huffman_code_tree(nodes[0][0])
+
+    print(' Char | Huffman code ')
+    print('----------------------')
+    for (char, frequency) in freq:
+        print(' %-4r |%12s' % (char, huffmanCode[char]))
+
+    if flag_z:
         print("optional parameter selected")
